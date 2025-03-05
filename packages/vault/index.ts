b3406395e30b60d5bb3e03dffad683222594710f
Andre <andre@tos.world>
2025-03-05 16:02:18 -0300

    update: v0.8.30

--- packages/vault/index.ts
+++ packages/vault/index.ts
@@ -1,5 +1,6 @@
 export * from './vault.config';
 export * from './vault.contract';
 export * from './vault.controller';
+export * from './vault.interface';
 export * from './vault.module';
 export * from './vault.service';
