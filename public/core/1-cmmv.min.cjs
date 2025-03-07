5cf599885cbd42707cf6b77ac96665179905dc2d
Andre <andre@tos.world>
2025-03-07 16:10:20 -0300

    fix: remove depreced dependencies view

--- public/core/1-cmmv.min.cjs
+++ public/core/1-cmmv.min.cjs
@@ -1,6 +1,6 @@
 /*!
  * cmmv.io (c) 2024, Andre Ferreira
- * compiled Fri, 07 Mar 2025 19:04:49 GMT
+ * compiled Fri, 07 Mar 2025 19:09:40 GMT
  * licensed under the MIT license
  * see: https://github.com/cmmvio/cmmv for details
  */
