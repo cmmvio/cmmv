7edda10d178e84977bcfbefef753b98d734e94ea
Andre <andre@tos.world>
2025-03-14 07:09:12 -0300

    fix: fingerprint generation

--- packages/http/lib/http.utils.ts
+++ packages/http/lib/http.utils.ts
@@ -4,11 +4,11 @@ import { URL } from 'node:url';
 export function generateFingerprint(req, usernameHashed) {
     const userAgent = req.headers['user-agent'] || '';
     const ip = req.ip || req.connection?.remoteAddress || '';
-    const accept = req.headers['accept'] || '';
     const language = req.headers['accept-language'] || '';
     const referer = req.headers['referer']
         ? new URL(req.headers['referer']).origin
         : '';
-    const rawFingerprint = `${userAgent}|${ip}|${accept}|${language}|${referer}|${usernameHashed}`;
+
+    const rawFingerprint = `${userAgent}|${ip}|${language}|${referer}|${usernameHashed}`;
     return crypto.createHash('sha256').update(rawFingerprint).digest('hex');
 }
