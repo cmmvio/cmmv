5a51f2cedf60ae6ade46e23df0955bd336ac7e8b
Andre Ferreira <andrehrf@gmail.com>
2025-05-13 06:05:14 -0300

    fix: remove scheduling logs

--- packages/core/managers/scheduling.manager.ts
+++ packages/core/managers/scheduling.manager.ts
@@ -6,63 +6,37 @@ type CronObserver = () => void;
 export class SchedulingManager {
     private cronJob: CronJob;
     private observers: CronObserver[] = [];
-    private logger: Logger = new Logger('SchedulingManager');
 
     constructor(cronTime: string, onTick: () => void) {
         this.cronJob = new CronJob(cronTime, () =>
             this.notifyObservers(onTick),
         );
-
-        if (process.env.NODE_ENV === 'dev')
-            this.logger.log(`Initialized cron job with schedule: ${cronTime}`);
     }
 
     public start(): void {
-        if (!this.cronJob.running) {
-            this.cronJob.start();
-
-            if (process.env.NODE_ENV === 'dev')
-                this.logger.log(`Cron job started`);
-        }
+        if (!this.cronJob.running) this.cronJob.start();
     }
 
     public stop(): void {
-        if (this.cronJob.running) {
-            this.cronJob.stop();
-
-            if (process.env.NODE_ENV === 'dev')
-                this.logger.log(`Cron job stopped`);
-        }
+        if (this.cronJob.running) this.cronJob.stop();
     }
 
     public restart(): void {
         this.stop();
         this.start();
-
-        if (process.env.NODE_ENV === 'dev')
-            this.logger.log(`Cron job restarted`);
     }
 
     public changeCronTime(newCronTime: string): void {
         this.cronJob.setTime(new CronTime(newCronTime));
         this.restart();
-
-        if (process.env.NODE_ENV === 'dev')
-            this.logger.log(`Cron job time changed to: ${newCronTime}`);
     }
 
     public addObserver(observer: CronObserver): void {
-        this.observers.push(observer);
-
-        if (process.env.NODE_ENV === 'dev')
-            this.logger.log(`Observer added to cron job`);
+        if (!this.observers.includes(observer)) this.observers.push(observer);
     }
 
     private notifyObservers(originalOnTick: () => void): void {
         originalOnTick();
         this.observers.forEach((observer) => observer());
-
-        if (process.env.NODE_ENV === 'dev')
-            this.logger.log('Notified all observers after cron execution');
     }
 }
